6355f36241bf94c53bf053fc2d8bf44d3339cbc4
Devin Dreszer <devindreszer@gmail.com>
2014-07-19 01:13:54 -0400

    Banner has call to action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :projects, only: [:index, :show]
+  resources :projects, only: [:index, :show, :create, :new]
   root "projects#index"
 end
